dd89ba331509aa55d3f4f25fe4663b18c387905e
ryanchall99 <rileychall@hotmail.co.uk>
2024-04-10 14:15:43 +0100

    Idle / Walking Transition
    Using a boolean parameter (IsWalking) on the units animator controller, the unit is now able to transition from an idle animation to a walking animation and back again.
    
    This is currently done via the unit code, and is updated when the character moves.
    
    Will hopefully get around to splitting the animations code from the movement code.

--- TurnBasedGame/Assets/Scripts/Unit.cs
+++ TurnBasedGame/Assets/Scripts/Unit.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    private const string IS_WALKING = "IsWalking";
+
+    [SerializeField] private Animator unitAnimator;
 
     private Vector3 targetPosition;
 
     private void Update() {
-        float stoppingDistance = .1f;
 
+        float stoppingDistance = .1f;
         if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance) {
             Vector3 moveDirection = (targetPosition - transform.position).normalized; // Move Direction (No Magnitude)
             float moveSpeed = 4f;
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+            unitAnimator.SetBool(IS_WALKING, true); // Update Animation (Walking)
+        } else {
+            unitAnimator.SetBool(IS_WALKING, false); // Update Animation (Idle)
         }
 
         if (Input.GetMouseButtonDown(0)) {
