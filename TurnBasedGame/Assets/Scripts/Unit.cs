da79bf586a2f21acdde41c1cdc79c35d3eff2872
ryanchall99 <rileychall@hotmail.co.uk>
2024-04-10 14:40:35 +0100

    Unit Rotation & HandleMovement Function
    - Moved all functionality for movement out of Update into own function called HandleMovement (Called Within Update).
    
    - Added unit rotation. The unit now rotates to movement direction using linear interpolation to create a smoother rotation.

--- TurnBasedGame/Assets/Scripts/Unit.cs
+++ TurnBasedGame/Assets/Scripts/Unit.cs
@@ -6,22 +6,18 @@ public class Unit : MonoBehaviour
 {
     private const string IS_WALKING = "IsWalking";
 
+    [Header("Movement & Rotation")]
+    [SerializeField] float moveSpeed = 4f;
+    [SerializeField] float rotateSpeed = 10f;
+
+    [Header("Animation")]
     [SerializeField] private Animator unitAnimator;
 
     private Vector3 targetPosition;
 
     private void Update() {
 
-        float stoppingDistance = .1f;
-        if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance) {
-            Vector3 moveDirection = (targetPosition - transform.position).normalized; // Move Direction (No Magnitude)
-            float moveSpeed = 4f;
-            transform.position += moveDirection * moveSpeed * Time.deltaTime;
-
-            unitAnimator.SetBool(IS_WALKING, true); // Update Animation (Walking)
-        } else {
-            unitAnimator.SetBool(IS_WALKING, false); // Update Animation (Idle)
-        }
+        HandleMovement();
 
         if (Input.GetMouseButtonDown(0)) {
             Move(MouseWorld.GetPosition());
@@ -30,5 +26,20 @@ public class Unit : MonoBehaviour
 
     private void Move(Vector3 targetPosition) {
         this.targetPosition = targetPosition; // Setting Member Variable
+    }
+
+    private void HandleMovement() {
+        float stoppingDistance = .1f;
+        if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance) {
+            Vector3 moveDirection = (targetPosition - transform.position).normalized; // Move Direction (No Magnitude)
+            transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed); // Unit Faces towards movement direction
+
+            unitAnimator.SetBool(IS_WALKING, true); // Update Animation (Walking)
+        }
+        else {
+            unitAnimator.SetBool(IS_WALKING, false); // Update Animation (Idle)
+        }
     }
 }
