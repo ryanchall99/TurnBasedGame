b72a3ff28cb9335e6d40e7b1f88718139b4d957b
ryanchall99 <rileychall@hotmail.co.uk>
2024-04-22 17:29:31 +0100

    Created GridObject & Debug Prefab
    - Created a GridObject class which will store all future data related to a specific grid position.
    
    - Created a prefab for debugging, which is instantiated in every grid position.

---
+++ TurnBasedGame/Assets/Scripts/Grid/GridObject.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridObject
+{
+
+    private GridSystem gridSystem;
+    private GridPosition gridPosition;
+
+    public GridObject(GridSystem gridSystem, GridPosition gridPosition)
+    {
+        this.gridSystem = gridSystem; // System Grid Is Using
+        this.gridPosition = gridPosition; // Grid Position Of Object
+    }
+}
--- TurnBasedGame/Assets/Scripts/Grid/GridSystem.cs
+++ TurnBasedGame/Assets/Scripts/Grid/GridSystem.cs
@@ -7,6 +7,7 @@ public class GridSystem
     private int width;
     private int height; // "Length" Used Height Due To Visualising in 2D
     private float cellSize;
+    private GridObject[,] gridObjectArray;
 
     public GridSystem(int width, int height, float cellSize) 
     {
@@ -15,11 +16,14 @@ public class GridSystem
         this.height = height;
         this.cellSize = cellSize;
 
+        gridObjectArray = new GridObject[width, height];
+
         for (int x = 0; x < width; x++) 
         {
             for (int z = 0; z < height; z++) 
             {
-                Debug.DrawLine(GetWorldPosition(x, z), GetWorldPosition(x, z) + Vector3.right * .2f, Color.white, 1000);
+                GridPosition gridPosition = new GridPosition(x, z);
+                gridObjectArray[x, z] = new GridObject(this, gridPosition); // Storing 2D Array
             }
         }
     }
@@ -35,5 +39,16 @@ public class GridSystem
             Mathf.RoundToInt(worldPosition.x / cellSize),
             Mathf.RoundToInt(worldPosition.z / cellSize)
         );
+    }
+
+    public void CreateDebugObjects(Transform debugPrefab)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+                GameObject.Instantiate(debugPrefab, GetWorldPosition(x, z), Quaternion.identity); // Instantiate Debug Prefab On Each Grid Position
+            }
+        }
     }
 }
